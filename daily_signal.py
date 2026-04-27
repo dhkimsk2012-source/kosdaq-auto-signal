3f89192b6c7d4cefccf721189078cfb43964e59f
dhkimsk2012-source <dhkimsk2012@gmail.com>
2026-04-27 22:52:12 +0900

    Update daily_signal.py

--- daily_signal.py
+++ daily_signal.py
@@ -100,10 +100,10 @@ def make_features(df):
 
     df["target"] = (df["next_close"] > df["next_open"]).astype(int)
 
-df = df.replace([np.inf, -np.inf], np.nan)
-df = df.dropna()
+    df = df.replace([np.inf, -np.inf], np.nan)
+    df = df.dropna()
 
-return df
+    return df
 
 # 모델
 def train_model(df):
